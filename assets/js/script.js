470c2042cdb5feb396de86eaaed00c104594a0e2
exquis22 <exquiswatches.ke@gmail.com>
2024-10-17 09:58:57 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,3 +224,19 @@ themeToggleButton.addEventListener('click', () => {
         localStorage.setItem('theme', 'light'); // Save the current theme in localStorage
     }
 });
+
+// resume code
+function toggleMenu() {
+    const navbarLinks = document.getElementById('navbarLinks');
+    navbarLinks.classList.toggle('active');
+}
+// Function to open the resume modal
+function openResume() {
+    document.getElementById('resumeModal').style.display = 'block';
+}
+
+// Function to close the resume modal
+function closeResume() {
+    document.getElementById('resumeModal').style.display = 'none';
+}
+
